2c210f13f3c0c577e6e6c29233d99627efef9d05
Michael <23063073+rushan-mike@users.noreply.github.com>
2023-04-13 11:40:17 +0530

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -4,6 +4,29 @@ $credential = New-Object System.Management.Automation.PSCredential($username, $p
 
 Test-WSMan -ComputerName 192.168.1.68 -Credential $credential -Authentication Negotiate
 
+
 Get-Item -Path WSMan:\localhost\Client\TrustedHosts
 
 Set-Item -Path WSMan:\localhost\Client\TrustedHosts -Value "192.168.1.68"
+
+
+Enable-WSManCredSSP -Role Client -DelegateComputer *
+
+Enable-WSManCredSSP -Role Server
+
+
+winrm s winrm/config/client '@{TrustedHosts="192.168.1.64,192.168.1.68"}'
+
+winrm g winrm/config/client
+
+
+Get-Item WSMan:\localhost\Service\AllowRemoteAccess
+
+Get-Item WSMan:\localhost\Service\Auth\CredSSP
+
+
+Get-ChildItem -Path Cert:\CurrentUser\My
+
+Get-ChildItem -Path WSMan:\localhost\Service\Auth\
+
+Set-Item -Path WSMan:\localhost\Service\Auth\CertificateThumbprint -Value <thumbprint>
